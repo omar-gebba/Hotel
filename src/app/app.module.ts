10f7f612b8333c16a493506072bdc125b8e76c09
omar-gebba <omargebba@gmail.com>
2020-02-14 03:52:17 +0200

    Adding angular materials-matBadge and matButton- and creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import  { BrowserModule }           from '@angular/platform-browser';
+import { NgModule }                 from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule }         from './app-routing.module';
+import { AppComponent }             from './app.component';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+import { MatSliderModule }          from '@angular/material/slider';
+import { MaterialModule }           from './material.module';     /// fore angular material modules
+import { AccomodationComponent }    from './components/accomodation/accomodation.component';
+import { RoomsComponent }           from './components/accomodation/rooms/rooms.component';
+import { FoodBeverageComponent }    from './components/food-beverage/food-beverage.component';
+import { RestaurantComponent }      from './components/food-beverage/restaurant/restaurant.component';
+import { DrinksComponent }          from './components/food-beverage/drinks/drinks.component';
+import { LifestyleComponent }       from './components/lifestyle/lifestyle.component';
+import { WelnessAndSpaComponent }   from './components/lifestyle/welness-and-spa/welness-and-spa.component';
+import { ResortActivitiesComponent} from './components/lifestyle/resort-activities/resort-activities.component';
+import { ExperiencesComponent }     from './components/lifestyle/experiences/experiences.component';
+import { ExcursionsComponent }      from './components/lifestyle/excursions/excursions.component';
+import { WeddingsComponent }        from './components/weddings/weddings.component';
+import { ThaiWeddingsComponent }    from './components/weddings/thai-weddings/thai-weddings.component';
+import { WesternsWeddingsComponent} from './components/weddings/westerns-weddings/westerns-weddings.component';
+import { CelebrationsComponent }    from './components/celebrations/celebrations.component';
+import { OffersComponent }          from './components/offers/offers.component';
+import { AuthComponent }            from './components/auth/auth.component';
+import { SigninComponent }          from './components/auth/signin/signin.component';
+import { SignupComponent }          from './components/auth/signup/signup.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccomodationComponent,
+    RoomsComponent,
+    FoodBeverageComponent,
+    RestaurantComponent,
+    DrinksComponent,
+    LifestyleComponent,
+    WelnessAndSpaComponent,
+    ResortActivitiesComponent,
+    ExperiencesComponent,
+    ExcursionsComponent,
+    WeddingsComponent,
+    ThaiWeddingsComponent,
+    WesternsWeddingsComponent,
+    CelebrationsComponent,
+    OffersComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
